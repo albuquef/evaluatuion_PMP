a98e6537f04d0d071146b2c9bf5d58739f08ac4f
felipeealb <felipe.albuquerquebs@gmail.com>
2024-06-25 00:00:02 +0200

    local eval solution

--- src/lit_eval_solutions.py
+++ src/lit_eval_solutions.py
@@ -20,18 +20,45 @@ def read_customer_assignments(filename):
     assignments = {}
     with open(filename, 'r') as file:
         lines = file.readlines()
+        
+        # Flag to identify the section of the file
+        in_location_assignments = False
+        
         for line in lines:
             line = line.strip()
+            
             if not line or line.startswith("CUSTOMER ASSIGNMENTS"):
                 continue
-            if "->" in line:
-                customer_part, location_part = line.split("->")
-                try:
-                    customer = int(customer_part.split()[0])
-                    location = int(location_part.split()[0])
-                    assignments[customer] = location
-                except ValueError:
-                    continue
+            
+            # Handle the new format
+            if line == "P LOCATIONS":
+                in_location_assignments = False
+                continue
+            
+            if line == "LOCATION ASSIGNMENTS":
+                in_location_assignments = True
+                continue
+            
+            if not in_location_assignments:
+                if "->" in line:
+                    customer_part, location_part = line.split("->")
+                    try:
+                        customer = int(customer_part.split()[0])
+                        location = int(location_part.split()[0])
+                        assignments[customer] = location
+                    except ValueError:
+                        continue
+            else:
+                if ':' in line:
+                    location_part, customers_part = line.split(":")
+                    try:
+                        location = int(location_part.strip())
+                        customers = map(int, customers_part.split())
+                        for customer in customers:
+                            assignments[customer] = location
+                    except ValueError:
+                        continue
+    
     return assignments
 
 def read_assignments_and_clusters(filename):
@@ -61,11 +88,20 @@ def read_assignments_and_clusters(filename):
 def calculate_euclidean_distance(coord1, coord2):
     return math.sqrt((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2)
 
+def min_dit_bigger_than_zero(distances):
+    min_dist_diff_zero = max(distances)
+    for d in distances:
+        if d > 0 and d < min_dist_diff_zero:
+            min_dist_diff_zero = d
+    return min_dist_diff_zero
+
 def process_files(directory_data, directory_sols_method, directory_sol_lit, base_filenames):
     for base_filename in base_filenames:
         customer_data_file = os.path.join(directory_data, f'cust_weights_{base_filename}.txt')
-        assignment_files = glob.glob(os.path.join(directory_sols_method, f'test_lit_{base_filename}_p_*_RSSV_EXACT_CPMP_BIN.txt'))
-        solution_file = glob.glob(os.path.join(directory_sol_lit, f'solucao.{base_filename}_*.txt'))[0]
+        # assignment_files = glob.glob(os.path.join(directory_sols_method, f'test_lit_{base_filename}_p_*_RSSV_EXACT_CPMP_BIN.txt'))
+        assignment_files = glob.glob(os.path.join(directory_sols_method, f'test_lit_{base_filename}_p_*.txt'))
+        # solution_file = glob.glob(os.path.join(directory_sol_lit, f'solucao.{base_filename}_*.txt'))[0]
+        solution_file = glob.glob(os.path.join(directory_sol_lit, f'soluOPT.{base_filename}_*.txt'))[0]
 
         print(f"Processing files for base filename {base_filename}:")
         print(f"Customer Data File: {customer_data_file}")
@@ -92,16 +128,23 @@ def process_files(directory_data, directory_sols_method, directory_sol_lit, base
             total_distance = sum(distances)
             if distances:
                 max_distance = max(distances)
+                min_distance = min(distances)
+                min_dist_diff_zero = min_dit_bigger_than_zero(distances)
                 avg_distance = statistics.mean(distances)
                 stddev_distance = statistics.stdev(distances)
             else:
-                max_distance = avg_distance = stddev_distance = 0
+                max_distance = avg_distance = stddev_distance = min_distance = min_dist_diff_zero = 0
 
             print(f"Total Euclidean Distance: {total_distance}")
             print(f"Maximum Distance: {max_distance}")
+            print(f"Minimum Distance: {min_distance}")
+            print(f"Minimum Distance Different from Zero: {min_dist_diff_zero}")
             print(f"Average Distance: {avg_distance}")
             print(f"Standard Deviation of Distances: {stddev_distance}")
+            print('-' * 50)
 
+
+        print('-' * 50)
         print(f"Solution File: {solution_file}")
 
         # Read solution data
@@ -120,6 +163,8 @@ def process_files(directory_data, directory_sols_method, directory_sol_lit, base
         total_distance = sum(distances)
         if distances:
             max_distance = max(distances)
+            min_distance = min(distances)
+            min_dist_diff_zero = min_dit_bigger_than_zero(distances)
             avg_distance = statistics.mean(distances)
             stddev_distance = statistics.stdev(distances)
         else:
@@ -127,6 +172,8 @@ def process_files(directory_data, directory_sols_method, directory_sol_lit, base
 
         print(f"Total Euclidean Distance: {total_distance}")
         print(f"Maximum Distance: {max_distance}")
+        print(f"Minimum Distance: {min_distance}")
+        print(f"Minimum Distance Different from Zero: {min_dist_diff_zero}")
         print(f"Average Distance: {avg_distance}")
         print(f"Standard Deviation of Distances: {stddev_distance}")
 
@@ -135,11 +182,16 @@ def process_files(directory_data, directory_sols_method, directory_sol_lit, base
         print()
 
 def main():
-    directory_data = '/home/falbuquerque/Documents/projects/Project_PMP/large-PMP/data/Literature/group5/'
-    directory_sols_method = '/home/falbuquerque/Documents/projects/Project_PMP/saves/SaveCluster/24-06-24_save_cluster/Literature_test_2/outputs/solutions/2024-06-24_LIT/Assignments'
-    directory_sol_lit = '/home/falbuquerque/Documents/projects/Project_PMP/large-PMP/data/Literature/solutions_lit/'
-    base_filenames = ['pr2392_020', 'pr2392_075', 'pr2392_150', 'pr2392_300', 'pr2392_500']
-
+    # directory_data = '/home/falbuquerque/Documents/projects/Project_PMP/large-PMP/data/Literature/group5/'
+    # directory_sol_lit = '/home/falbuquerque/Documents/projects/Project_PMP/large-PMP/data/Literature/solutions_lit/'
+    # directory_sols_method = '/home/falbuquerque/Documents/projects/Project_PMP/saves/SaveCluster/24-06-24_save_cluster/Literature_test_2/outputs/solutions/2024-06-24_LIT/Assignments'
+    # base_filenames = ['pr2392_020', 'pr2392_075', 'pr2392_150', 'pr2392_300', 'pr2392_500']
+    
+    directory_data = '/home/felipe/Documents/Projects/GeoAvigon/pmp_code/large-PMP/data/Literature/group2/'
+    directory_sol_lit = '/home/felipe/Documents/Projects/GeoAvigon/pmp_code/large-PMP/data/Literature/solutions_lit/'
+    directory_sols_method = '/home/felipe/Documents/Projects/GeoAvigon/pmp_code/large-PMP/outputs/solutions/2024-06-24_LIT/Assignments'
+    base_filenames = ['SJC1']
+    
     process_files(directory_data, directory_sols_method, directory_sol_lit, base_filenames)
 
 if __name__ == "__main__":
